a93a1effe7f35c63925fa90e23a2cf6341721345
Adrián Galinsoga <116064067+gvlinsoga@users.noreply.github.com>
2023-11-29 17:08:47 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ images.g.ts
@@ -0,0 +1,344 @@
+// Código generado automáticamente. No editar.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . b b b . . . . . . . 
+. . . . . . b 9 b . . . . . . . 
+. . . . . . b 9 b . . . . . . . 
+. . . . . . b 9 b b . . . . . . 
+. . . . . . b 9 b b . . . . . . 
+. . . . . b b 9 9 b . . . . . . 
+. . . . . b 9 9 9 b b . . . . . 
+. . . . . b 9 9 9 9 b . . . . . 
+. . . . b 9 1 9 9 1 9 b . . . . 
+. . . . b 9 f 9 9 f 9 b . . . . 
+. . . . b 9 9 9 9 9 9 b . . . . 
+. . . . b 9 9 3 3 9 9 b . . . . 
+. . . . . b 9 9 9 9 b . . . . . 
+. . . . . b b b b b b . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "myImage0":return img`
+. . . . . b b b b b b . . . . . 
+. . . b b 9 9 9 9 9 9 b b . . . 
+. . b b 9 9 9 9 9 9 9 9 b b . . 
+. b b 9 d 9 9 9 9 9 9 9 9 b b . 
+. b 9 d 9 9 9 9 9 1 1 1 9 9 b . 
+b 9 d d 9 9 9 9 9 1 1 1 9 9 9 b 
+b 9 d 9 9 9 9 9 9 1 1 1 9 9 9 b 
+b 9 d 9 9 9 9 9 9 9 9 9 1 9 9 b 
+b 9 d 9 9 9 9 9 9 9 9 9 9 9 9 b 
+b 9 d 9 9 9 1 9 9 9 9 9 9 d 9 b 
+b 9 d 9 1 1 9 9 9 9 9 9 d d 9 b 
+. b d 9 1 1 9 9 9 9 9 d d 9 b . 
+. b d 9 9 9 9 9 9 9 9 d 9 b b . 
+. . b d d 9 9 9 9 9 9 9 b b . . 
+. . . b b d d 9 9 9 9 b b . . . 
+. . . . . b b b b b b . . . . . 
+`;
+            case "image3":
+            case "Zombie 1":return img`
+. . . . f f f f . . . . 
+. . f f e e e e f f . . 
+. f f e e e e e e f f . 
+f f f f 7 e e e f f f f 
+f f f 7 7 7 e e f f f f 
+f f f 7 7 7 7 e e f f f 
+f 7 e 7 7 7 7 7 7 e 7 f 
+f 7 7 f f 7 7 f f 7 7 f 
+f e 7 7 7 7 7 7 7 7 e f 
+. f e 7 7 2 2 7 7 e f . 
+. f f e 7 7 7 7 e f f . 
+e 7 f b 8 8 8 8 b f 7 e 
+7 d f 8 8 8 8 8 8 f d 7 
+7 7 f 6 6 6 6 6 6 f 7 7 
+. . . f f f f f f . . . 
+. . . f f . . f f . . . 
+`;
+            case "image4":
+            case "zombie2":return img`
+. . . . . f f f f . . . . . 
+. . . f f 5 5 5 5 f f . . . 
+. . f 5 5 5 5 5 5 5 5 f . . 
+. f 5 5 5 5 5 5 5 5 5 5 f . 
+. f 5 f f f f f f f f 5 f . 
+f 5 5 f 7 7 7 7 7 7 f 5 5 f 
+f 5 5 f 7 7 7 7 7 7 f 5 5 f 
+f 5 5 f 7 f 7 7 f 7 f 5 5 f 
+f 5 5 f 7 f 7 7 f 7 f 5 5 f 
+. f 5 f 7 7 7 7 7 7 f 5 f . 
+. f f f f f f f f f f f f . 
+. e 7 f 6 a a a a 6 f 7 e . 
+. 7 d b a a a a a a b d 7 . 
+. 7 f 3 3 3 3 3 3 3 3 f 7 . 
+. . f f 3 3 3 3 3 3 f f . . 
+. . . . f f 3 3 f f . . . . 
+`;
+            case "image5":
+            case "zombie3":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e 2 2 2 2 2 2 e e f . . 
+. . f e 2 f f f f f f 2 e f . . 
+. . f f f f e e e e f f f f . . 
+. f f e f 7 f 7 7 f 7 f e f f . 
+. f e e 7 7 f 7 7 f 7 7 e e f . 
+. . f e e 7 7 7 7 7 7 e e f . . 
+. f d d e e 7 7 7 7 e e f . . . 
+. f e 7 f 2 2 2 2 2 2 f 7 e . . 
+. f 7 d f 2 2 2 2 2 2 f d 7 . . 
+. . 7 7 f f 7 5 5 7 7 f 7 7 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "zombie moviment":
+            case "anim1":return [img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f b b f f f . . . . 
+. . . f f f b b b b f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e b b b b b b e e f . . 
+. . f e b f f f f f f b e f . . 
+. . f f f f e e e e f f f f . . 
+. f f e f 7 f 7 7 f 7 f e f f . 
+. f e e 7 7 f 7 7 f 7 7 e e f . 
+. . f e e 7 7 7 7 7 7 e e f . . 
+. . . f e e 7 7 7 7 e e f . . . 
+. . e 7 f b b b b b b f 7 e . . 
+. . 7 d f b b b b b b f d 7 . . 
+. . 7 7 f 7 7 5 5 7 7 f 7 7 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . f f f b b f f f . . . . 
+. . . f f f b b b b f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e b b b b b b e e f . . 
+. f f e b f f f f f f b e f f . 
+. f f f f f e e e e f f f f f . 
+. . f e f 7 f 7 7 f 7 f e f . . 
+. . f e 7 7 f 7 7 f 7 7 e f . . 
+. . . f e 7 7 7 7 7 7 e f e . . 
+. . f e f b b b b b 7 7 7 e . . 
+. . e 7 f b b b b e 7 7 e . . . 
+. . . . f 7 7 5 5 f e e . . . . 
+. . . . f f f f f f f . . . . . 
+. . . . f f f . . . . . . . . . 
+`, img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f b b f f f . . . . 
+. . . f f f b b b b f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e b b b b b b e e f . . 
+. . f e b f f f f f f b e f . . 
+. . f f f f e e e e f f f f . . 
+. f f e f 7 f 7 7 f 7 f e f f . 
+. f e e 7 7 f 7 7 f 7 7 e e f . 
+. . f e e 7 7 7 7 7 7 e e f . . 
+. . . f e e 7 7 7 7 e e f . . . 
+. . e 7 f b b b b b b f 7 e . . 
+. . 7 d f b b b b b b f d 7 . . 
+. . 7 7 f 7 7 5 5 7 7 f 7 7 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . f f f b b f f f . . . . 
+. . . f f f b b b b f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f e e b b b b b b e f f . . 
+. f f e b f f f f f f b e f f . 
+. f f f f f e e e e f f f f f . 
+. . f e f 7 f 7 7 f 7 f e f . . 
+. . f e 7 7 f 7 7 f 7 7 e f . . 
+. . e f e 7 7 7 7 7 7 e f . . . 
+. . e 7 7 7 e b b b b f e f . . 
+. . . e 7 7 e b b b b f 7 e . . 
+. . . . e e f 5 5 7 7 f . . . . 
+. . . . . f f f f f f f . . . . 
+. . . . . . . . . f f f . . . . 
+`];
+            case "zombiecalavera":
+            case "anim2":return [img`
+........................
+........................
+........................
+........................
+........................
+..........ffff..........
+........ff1111ff........
+.......fb111111bf.......
+.....fffc1111111f.......
+...fc111cd1111111f......
+...f1b1b1b1111dddf......
+...fbfbffc2112cddf......
+......fcf111111bbf......
+.......ccbdb1b1fcf......
+.......fffbfbfdff.......
+........ffffffff........
+........fffffffffff.....
+.........fffffc111cf....
+.........fffff1b1b1f....
+..........ffffbfbfbf....
+...........ffff.........
+........................
+........................
+........................
+`, img`
+........................
+........................
+........................
+........................
+........................
+..........ffff..........
+........ff1111ff........
+.......fb111111bf.......
+.......f11111111f.......
+......fd11111111df......
+....7.fd11111111df......
+...7..fd11111111df......
+...7..fd11111111df......
+...7..fddd1111dddff.....
+...77.fbdb2dd2bdbfcf....
+...777fcdc2112cdcfbf....
+....77fffbdb1bdffcf.....
+....fcb1bcffffff........
+....f1c1c1ffffff........
+....fdfdfdfffff.........
+.....f.f.f..............
+........................
+........................
+........................
+`, img`
+........................
+........................
+........................
+........................
+..........ffff..........
+........ff1111ff........
+.......fb111111bf.......
+.......f11111111f.......
+......fd111111111f......
+......fd11111111df......
+......fd11111111df......
+......fcdd1111ddcff.....
+.......fbc2112cbfbbf....
+.......ffbdb1bdffff.....
+........fcbfbfdf........
+........ffffffff........
+......ffffffffff........
+.....fcb1bcffff.........
+......ffbff.............
+........................
+........................
+........................
+........................
+........................
+`, img`
+........................
+........................
+........................
+........................
+..........ffff..........
+........ff1111ff........
+.......fb111111bf.......
+.......f11111111f.......
+......fd11111111df......
+......fdd111111ddf......
+......fbdd1111dddf......
+......fcdb2dd2bdbf......
+.......fbc2112cbfff.....
+.......ffb1111bcfbcf....
+........fcdb1bdfbbbf....
+.......ffffffffffcf.....
+.....fcb1bcfffff........
+.....f1b1b1ffff.........
+......ffbff.............
+........................
+........................
+........................
+........................
+........................
+`];
+            case "myAnim":
+            case "anim3":return [img`
+. f f f . f f f f . f f f . 
+f f f f f c c c c f f f f f 
+f f f f b c c c c b f f f f 
+f f f c 3 c c c c 3 c f f f 
+. f 3 3 c c c c c c 3 3 f . 
+. f c c c c 7 7 c c c c f . 
+. f f c c 7 7 7 7 c c f f . 
+. f f f b f 7 7 f b f f f . 
+. f f 7 1 f 7 7 f 1 7 f f . 
+. . f f 7 7 7 7 7 7 f f . . 
+. . e f e 7 2 2 7 e f e . . 
+. e 7 f b 3 3 2 3 b f 7 e . 
+. 7 d f 3 3 3 2 2 3 c d 7 . 
+. 7 7 f 6 6 6 6 2 6 f 7 7 . 
+. . . . f f f f f f . . . . 
+. . . . f f . . f f . . . . 
+`, img`
+. . . . . . . . . . . . . . 
+. f f f . f f f f . f f f . 
+f f f f f c c c c f f f f f 
+f f f f b c c c c b f f f f 
+f f f c 3 c c c c 3 c f f f 
+. f 3 3 c c c c c c 3 3 f . 
+. f c c c c 7 7 c c c c f . 
+. f f c c 7 7 7 7 c c f f . 
+. f f f b f 4 4 f b f f f . 
+. f f 7 1 f 7 7 f 1 7 f f . 
+. . f f 7 7 2 2 7 7 e f e . 
+. f e f f b b 2 e 7 7 7 e . 
+. e 7 f b 3 2 3 e 7 7 e . . 
+. . . f 6 2 6 6 f e e . . . 
+. . . f f f f f f f . . . . 
+. . . f f f . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . 
+. f f f . f f f f . f f f . 
+f f f f f c c c c f f f f f 
+f f f f b c c c c b f f f f 
+f f f c 3 c c c c 3 c f f f 
+. f 3 3 c c c c c c 3 3 f . 
+. f c c c c 7 7 c c c c f . 
+. f f c b 7 7 7 7 b c f f . 
+. f f f b f 7 7 f b f f f . 
+. f f 7 1 f 7 7 f 1 7 f f . 
+. e f e 7 7 7 7 7 7 f f . . 
+. e 4 d d e b b b f f e f . 
+. . e d d e 3 3 b e f 7 e . 
+. . . e e f 6 6 6 6 f . . . 
+. . . . f f f f f f f . . . 
+. . . . . . . . f f f . . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Código generado automáticamente. No editar.
